d17a3475257f24a12d68cd15b4eb2beaae0f39b0
CristhianFSantos <cristhianfelipe0@gmail.com>
2021-09-15 14:12:15 -0300

    ✨ feat: app component layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'dev-finder';
+  mode = ['DARK', 'LIGHT'];
+  darkMode = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  currentUser: User;
+
+  updateUser(user: User) {
+    this.currentUser = user;
+  }
+
+  changeMode() {
+    this.darkMode = !this.darkMode;
+    this.document.documentElement.classList.toggle('dark-mode');
+  }
 }
