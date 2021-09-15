c3048ba10b471509d442f40ef1ac208420d4b988
CristhianFSantos <cristhianfelipe0@gmail.com>
2021-09-15 14:43:40 -0300

    ✨ feat: add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchUsernameComponent } from './components/search-username/search-username.component';
+import { CardUserDetailsComponent } from './components/card-user-details/card-user-details.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchUsernameComponent,
+    CardUserDetailsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
